6492c47cfe6ed43969f98a7838d4acf5ec03650c
kshneo0 <kshneo@gmail.com>
2022-01-28 19:35:08 +0900

    sec3 ch4 Avoiding Side Effects and Using Pure Functions

---
+++ app.js
@@ -0,0 +1,99 @@
+// let cnt = 0
+
+// let increment = function() {
+//     cnt++;
+//     return cnt;
+// };
+
+// Pure Function
+let increment = function (num) {
+  return num + 1;
+};
+
+let average = function (scores) {
+  var total = 0;
+  for (let i = 0; i < scores.length; i++) {
+    total += scores[i];
+  }
+  return total / scores.length;
+};
+
+console.log(average([90, 30, 40, 50, 60]));
+
+/*var MAINAPP = (function(nsp) {
+    var currentUser = 0,
+        users = [{name: "James",score: 30,tries: 1}, {name: "Mary", score: 110,tries: 4}, {name: "Henry",score: 80,tries: 3}];
+
+    var updateScore = function(newAmt) {
+        users[currentUser].score += newAmt;
+    };
+
+    var returnUsers = function() {
+        return users;
+    };
+
+    var updateTries = function() {
+        users[currentUser].tries++;
+    };
+
+    var updateUser = function(newUser) {
+        currentUser = newUser;
+    };
+
+    nsp.updateUser = updateUser;
+    nsp.updateTries = updateTries;
+    nsp.updateScore = updateScore;
+    nsp.returnUsers = returnUsers;
+    return nsp;
+})(MAINAPP || {});
+
+setTimeout(function() {MAINAPP.updateUser(2);}, 300);
+setTimeout(function() {MAINAPP.updateScore(20);}, 100);
+setTimeout(function() {MAINAPP.updateTries();}, 200);*/
+
+var users = [
+  { name: "James", score: 30, tries: 1 },
+  { name: "Mary", score: 110, tries: 4 },
+  { name: "Henry", score: 80, tries: 3 },
+];
+
+// Modifies Data
+var storeUser = function (arr, user) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].name.toLowerCase() === user.name.toLowerCase()) {
+      arr[i] = user;
+      break;
+    }
+  }
+};
+
+// Pure Functions
+var getUser = function (arr, name) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].name.toLowerCase() === name.toLowerCase()) {
+      return arr[i];
+    }
+  }
+  return null;
+};
+
+var updateScore = function (user, newAmt) {
+  if (user) {
+    user.score += newAmt;
+    return user;
+  }
+};
+
+var updateTries = function (user) {
+  if (user) {
+    user.tries++;
+    return user;
+  }
+};
+
+let user = getUser(users, "Henry");
+let user1 = updateScore(user, 30);
+let user2 = updateTries(user1);
+storeUser(user2);
+
+console.log(users);
