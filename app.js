8215f2cb0e03147d2195999acedf96bb24e50180
kshneo0 <kshneo@gmail.com>
2022-01-28 20:57:43 +0900

    sec3 ch7 Other Possible Solutions and the Problems

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ let average = function (scores) {
 
 console.log(average([90, 30, 40, 50, 60]));
 
+//-----------------------------------------------------------------------
+
 /*var MAINAPP = (function(nsp) {
     var currentUser = 0,
         users = [{name: "James",score: 30,tries: 1}, {name: "Mary", score: 110,tries: 4}, {name: "Henry",score: 80,tries: 3}];
@@ -97,3 +99,36 @@ let user2 = updateTries(user1);
 storeUser(user2);
 
 console.log(users);
+//--------------------------------------------------
+
+//객체가 함수의 인자로 전달이 될 때는 call by reference
+//원시데이터가 함수의 인자로 전달이 될 때는 call by value
+
+var users = [
+  { name: "James", score: 30, tries: 1 },
+  { name: "Mary", score: 110, tries: 4 },
+  { name: "Henry", score: 80, tries: 3 },
+];
+
+var newScore = function (arr, name, amt) {
+  arr.forEach(function (val) {
+    if (val.name.toLowerCase() === name.toLowerCase()) {
+      val.score = cal.score + amt;
+    }
+  });
+  return arr;
+};
+
+var newTries = function (arr, name) {
+  arr.forEach(function (val) {
+    if (val.name.toLowerCase() === name.toLowerCase()) {
+      val.tries++;
+    }
+  });
+  return arr;
+};
+
+var newArray1 = newScore(users, "Henry", 30);
+var newArray2 = newTries(users, "Henry");
+
+// 따라서 원본 배열에 side effect가 발생
